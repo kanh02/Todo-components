713e735c07193ece0673df851cfc92efa390ddef
oscar <kajiryohji02@gmail.com>
2018-11-06 16:54:17 +0100

    exercise

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo">
+</div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import Todo from './components/Todo';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    Todo,
+    CreateTodo,
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false,
+      });
+    },
+  },
+  data(){
+    return{
+      todos:[{
+        title:`Todo A`,
+        project:`Project A`,
+        done: false,
+      },{
+        title:`Todo B`,
+        project:`Project B`,
+        done: false,
+      },{
+        title:`Todo C`,
+        project:`Project C`,
+        done: false,
+      },{
+        title:`Todo D`,
+        project:`Project D`,
+        done: false,
+      }]
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
